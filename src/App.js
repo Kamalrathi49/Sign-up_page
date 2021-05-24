50848d223a4f2f41e2fcca0c9380b2cfb7597fe1
Kamal Rathi <kamalrathi049@gmail.com>
2021-05-24 22:17:56 +0530

    fixing an error

--- src/App.js
+++ src/App.js
@@ -20,10 +20,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-        if (userAuth) {
-          const userRef = await createUserProfileDocument(userAuth)
-        }
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+        console.log(this.state)
+      }
     });
   }
 
